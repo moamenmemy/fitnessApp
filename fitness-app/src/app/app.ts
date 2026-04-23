1e3a868cffdb3e97b3177fbaabc7ceaac5dcfb20
moamen memy <memmoamen82@gmail.com>
2026-04-23 19:59:30 +0200

    fontawo

--- fitness-app/src/app/app.ts
+++ fitness-app/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  imports: [ButtonModule],
+  imports: [ButtonModule,FontAwesomeModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected title = 'fitness-app';
+    faCoffee = faCoffee;
+
 }
