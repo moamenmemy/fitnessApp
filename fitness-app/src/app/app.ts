9a1673e19b10d2059ea66aec02df12e82965fc55
Esraasamyzakria <esamy3955@gmail.com>
2026-05-02 22:05:57 +0300

    finish rebot and title

--- fitness-app/src/app/app.ts
+++ fitness-app/src/app/app.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { RouterOutlet } from '@angular/router';
 import { DrawerModule } from 'primeng/drawer';
+import { FloatingImageComponent } from "./shared/components/floating-image/floating-image.component";
 @Component({
-  imports: [ButtonModule, DrawerModule,  RouterOutlet],
+  imports: [ButtonModule, DrawerModule, RouterOutlet, FloatingImageComponent],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
--- fitness-app/src/app/shared/components/secrion-title.component.ts
+++ fitness-app/src/app/shared/components/secrion-title.component.ts
@@ -1,12 +1,14 @@
 import { Component, input } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-secrion-title',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './secrion-title.component.html',
   styleUrl: './secrion-title.component.css',
 })
 export class SecrionTitleComponent {
-  // title = input.required<string>(); 
-  // bgTitle = input<string>()
+  title = input.required<string>(); 
+  fadumbbell=faDumbbell
+  bgTitle = input<string>()
 }
