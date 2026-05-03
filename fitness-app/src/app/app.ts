440c57dfdd7e7111e61cd21ef25dd2a8a6ba655c
moamen memy <memmoamen82@gmail.com>
2026-05-03 17:50:38 +0300

    first commit for why us and services bar

--- fitness-app/src/app/app.ts
+++ fitness-app/src/app/app.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { ServicesBarComponent } from "./pages/home/components/services-bar/servicesBar.component";
+import { WhyUsComponent } from "./pages/home/components/whyUs/whyUs.component";
 
 @Component({
-  imports: [ButtonModule,FontAwesomeModule],
+  imports: [ButtonModule, FontAwesomeModule, ServicesBarComponent, WhyUsComponent],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
---
+++ fitness-app/src/app/pages/home/components/whyUs/whyUs.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-why-us',
+  imports: [],
+  templateUrl: './whyUs.component.html',
+  styleUrl: './whyUs.component.css',
+})
+export class WhyUsComponent {}
