6b9d8e944226406f4e0d0ab4d35b59651664e883
Esraasamyzakria <esamy3955@gmail.com>
2026-05-04 16:37:05 +0300

    download

---
+++ libs/auth/src/lib/auth/auth.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-auth',
+  imports: [],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css',
+})
+export class AuthComponent {}
