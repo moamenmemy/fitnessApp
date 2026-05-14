12c2e2139abc50e1f4cd0ce4e770314a8ca91f39
Esraasamyzakria <esamy3955@gmail.com>
2026-05-15 00:03:20 +0300

    finished

--- fitness-app/src/app/shared/components/button-ui/buttonUi.component.ts
+++ fitness-app/src/app/shared/components/button-ui/buttonUi.component.ts
@@ -3,14 +3,16 @@ import { BadgeModule } from 'primeng/badge';
 import { OverlayBadgeModule } from 'primeng/overlaybadge';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSagittarius } from '@fortawesome/free-solid-svg-icons';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-button-ui',
-  imports: [BadgeModule, OverlayBadgeModule, FontAwesomeModule],
+  imports: [BadgeModule, OverlayBadgeModule, FontAwesomeModule,RouterLink],
   templateUrl: './buttonUi.component.html',
   styleUrl: './buttonUi.component.css',
 })
 export class ButtonUiComponent {
   sagittarius=faSagittarius
+  route=input<string>('');
   text=input<string>('');
   textcolor=input<string>('');
  backgroundcolor = input<string>('transparent');
