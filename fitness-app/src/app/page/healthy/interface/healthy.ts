a9bbce3b19f189ad0d23c8bba635a0e9401a7479
Esraasamyzakria <esamy3955@gmail.com>
2026-06-23 14:45:22 +0300

    git pull momen

---
+++ fitness-app/src/app/page/healthy/interface/healthy.ts
@@ -0,0 +1,24 @@
+export interface ApiCategory {
+  idCategory: string;
+  strCategory: string;
+  strCategoryThumb: string;
+  strCategoryDescription: string;
+}
+
+export interface ApiMeal {
+  idMeal: string;
+  strMeal: string;
+  strMealThumb: string;
+}
+
+
+export interface CarouselItem {
+  idMeal: string;
+  title: string;
+  imageSrc: string;
+  subText?: string;
+} 
+
+export interface ApiMealsResponse {
+  meals: ApiMeal[] | null;
+}
---
+++ fitness-app/src/app/page/healthy/service/meals/meals.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { ApiCategory,ApiMealsResponse} from '../../interface/healthy';
+import { TabItem } from '../../../../Shareds/customTabs/interface/customTabs';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Meals {
+  private http = inject(HttpClient);
+  private baseUrl = 'https://www.themealdb.com/api/json/v1/1';
+
+  
+  getMealsByCategory(categoryName: string): Observable<ApiMealsResponse> {
+    return this.http.get<ApiMealsResponse>(`${this.baseUrl}/filter.php?c=${categoryName}`);
+  }
+
+  
+  getAllCategories(): Observable<TabItem[]> {
+    return this.http.get<{ categories: ApiCategory[] }>(`${`${this.baseUrl}/categories.php`}`).pipe(
+      map(response => {
+        const categoriesArray = response.categories || [];
+        return categoriesArray.map(cat => ({
+          id: cat.strCategory, 
+          text: cat.strCategory  
+        }));
+      })
+    );
+  }
+}
+  
